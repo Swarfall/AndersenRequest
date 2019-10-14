74dd6b7ac260c9403176df2bcdb8c90b882005cf
Viacheslav <savitsky,fit@gmail.com>
2019-10-14 10:24:49 +0300

    GCD commite

---
+++ AnderserRequest/AnderserRequest/GCD/Controller/DispatchTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DispatchTaskViewController.swift
+//  AnderserRequest
+//
+//  Created by admin on 07.10.2019.
+//  Copyright © 2019 Viacheslav Savitsky. All rights reserved.
+//
+
+import UIKit
+
+class DispatchTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
